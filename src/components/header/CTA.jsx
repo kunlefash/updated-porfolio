99d3282379bd6b927b60480ceb9ff0d69484e626
kunlefash <kunlefashmayop@gmail.com>
2022-03-13 22:20:00 -0300

    Worked on the header component and its media queries

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,11 @@
+import CV from "../../assets/CV.pdf"
+const CTA = () => {
+  return (
+    <div className="cta">
+        <a href={CV} download className="btn">Download CV</a>
+        <a href="#contact" className="btn btn-primary">Contact Us</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import "./header.css"
+import CTA from "./CTA"
+import ME from "../../../src/assets/me.png"
+import Socials from './Socials'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello there</h5>
+        <h1>I am Fasakin Adekunle</h1>
+        <h5><div className="text-light">Fullstack Developer</div></h5>
+        <CTA />
+        <Socials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+
+        </div>
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
