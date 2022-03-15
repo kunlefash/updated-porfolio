a8a0613a521ba0012b3993f551d7e9f85777a2fa
kunlefash <kunlefashmayop@gmail.com>
2022-03-15 19:10:48 -0300

    master

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,11 +6,13 @@ import {GrUserExpert} from "react-icons/gr"
 import {FcServices} from "react-icons/fc"
 import {FcContacts} from "react-icons/fc"
 
+import { useState } from 'react'
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
       <nav>
-        <a href="#"><FcHome/></a>
-        <a href="#about"><FcAbout/></a>
+        <a href="#" onClick={() => setActiveNav('#')}className={activeNav==='#' ? 'active' : ''}><FcHome/></a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className= {activeNav==="#about" ? "active": ""}><FcAbout/></a>
         <a href="#experience"><GrUserExpert/></a>
         <a href="#services"><FcContacts/></a>
         <a href="#contact"><FcContacts/></a>
